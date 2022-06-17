249b70fe30744cbc07865ab409dae2711495e816
DjRedstone <djredstone64@gmail.com>
2022-06-17 18:33:28 +0200

    Structure principale

---
+++ creator.js
@@ -0,0 +1,19 @@
+function addQuote() {
+
+}
+
+function removeQuote() {
+
+}
+
+function addAnswer(quoteNB, nb) {
+
+}
+
+function removeAnswer(quoteNB, nb) {
+
+}
+
+function download() {
+    window.alert("WIP")
+}
