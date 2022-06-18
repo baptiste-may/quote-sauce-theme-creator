0e7a236c579af48ba4a0c64b323a4a32868a81a0
DjRedstone <djredstone64@gmail.com>
2022-06-18 17:36:48 +0200

    Javascript pour le téléchargement

--- download.js
+++ download.js
@@ -1,11 +1,19 @@
-
-const fs = require('fs');
-const yaml = require('js-yaml');
-
 function download() {
   var element = document.createElement('a');
-  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent("name: \"" + document.getElementById("name").value + "\""));
-  element.setAttribute('download', document.getElementById("name").value);
+
+  let doc = "name: \"" + document.getElementById("name").value + "\"\n";
+  doc += "questions:\n";
+  for (i = 1; i <= quotes; i++) {
+    doc += "  - number: " + i + "\n";
+    doc += "    quote: \"" + document.getElementById("quote-" + i).value + "\"\n";
+    doc += "    aswer:\n"
+    for (j = 1; j <= answers[i-1]; j++) {
+      doc += "      - \"" + document.getElementById("answer-" + i + "-" + j).value + "\"\n";
+    }
+  }
+
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(doc));
+  element.setAttribute('download', document.getElementById("name").value + ".yaml");
 
   element.style.display = 'none';
   document.body.appendChild(element);
